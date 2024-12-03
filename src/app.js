b578a5e32feb075a137dfdcebe9b92156539aedf
LauraPostigo <166739141+LauraPostigo@users.noreply.github.com>
2024-12-03 22:00:17 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,16 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let palo = ["♣", "♠", "♥", "♦"];
+let value = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomPaloIndex = Math.floor(Math.random() * palo.length);
+  let randomValueIndex = Math.floor(Math.random() * value.length);
+
+  let selectedPalo = palo[randomPaloIndex];
+  let selectedValue = value[randomValueIndex];
+  document.querySelector(
+    ".card-mid"
+  ).textContent = `${selectedValue} ${selectedPalo}`;
 };
